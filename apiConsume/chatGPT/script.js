3bc7c8c2d0cff5966a5f30893d2c401ccdb329f2
Daniel García <workspacedaniel2121@gmail.com>
2024-09-12 14:26:46 -0400

    Listo calisto

--- apiConsume/chatGPT/script.js
+++ apiConsume/chatGPT/script.js
@@ -1,3 +1,5 @@
+import { CreateWebWorkerMLCEngine } from 'https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm';
+
 const $ = (el) => document.querySelector(el);
 
 const $form = $('form');
@@ -6,9 +8,36 @@ const $template = $('#message-template');
 const $messages = $('ul');
 const $container = $('main');
 const $button = $('button');
+const $info = $('small');
+const $loading = $('.loading');
 
-$form.addEventListener('submit', (e) => {
-  e.preventDefault();
+let messages = [];
+let end = false;
+
+const SELECTED_MODEL = 'Llama-3-8B-Instruct-q4f32_1-MLC-1k';
+
+const engine = await CreateWebWorkerMLCEngine(
+  new Worker('./worker.js', { type: 'module' }),
+  SELECTED_MODEL,
+  {
+    initProgressCallback: (info) => {
+      $info.textContent = info.text;
+      if (info.progress === 1 && !end) {
+        end = true;
+        $loading?.parentNode?.removeChild($loading);
+        $button.removeAttribute('disabled');
+        addMessage(
+          '¡Hola!, Soy un modelo de inteligencia artificial ejecutado desde tu navegador, preguntame lo que quieras',
+          'bot'
+        );
+        $input, focus();
+      }
+    },
+  }
+);
+
+$form.addEventListener('submit', async (event) => {
+  event.preventDefault();
   const messageText = $input.value.trim();
 
   if (messageText !== '') {
@@ -17,12 +46,38 @@ $form.addEventListener('submit', (e) => {
   }
 
   addMessage(messageText, 'user');
-  $button.setAttribute('disabled', true);
+  $button.setAttribute('disabled', '');
 
-  setTimeout(() => {
-    addMessage('Ola k ase', 'bot');
-    $button.removeAttribute('disabled');
-  }, 2000);
+  const userMessage = {
+    role: 'user',
+    content: messageText,
+  };
+
+  messages.push(userMessage);
+
+  const chunks = await engine.chat.completions.create({
+    messages,
+    stream: true,
+  });
+
+  let reply = '';
+
+  const $botMessage = addMessage('', 'bot');
+
+  for await (const chunk of chunks) {
+    const choice = chunk.choices[0];
+    // optional chaining, en el caso que sea null || undefined, recibe texto vacio
+    const content = choice?.delta?.content ?? '';
+    reply += content;
+    $botMessage.textContent = reply;
+  }
+
+  $button.removeAttribute('disabled');
+  messages.push({
+    role: 'assistant',
+    content: reply,
+  });
+  $container.scrollTop = $container.scrollHeight;
 });
 
 function addMessage(text, sender) {
@@ -41,4 +96,7 @@ function addMessage(text, sender) {
 
   // // Actualizar el scroll
   $container.scrollTop = $container.scrollHeight;
+
+  // elemento donde escribimos la respuesta
+  return $text;
 }
